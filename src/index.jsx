102fae442d179d745bcefb1e3d843ec15727cc29
coreysf <corey.j.roy@gmail.com>
2016-09-03 19:14:59 -0700

    Add basic component structure for MVP quiz

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,45 @@
 import React from 'react'
 import { render } from 'react-dom'
+import Quiz from './Quiz/Quiz.jsx'
+import GenreList from './GenreList/GenreList.jsx'
+
 require('./style.css')
 
 const mountNode = document.getElementById('root')
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showQuiz: false
+    }
+    this.genreToggle = this.genreToggle.bind(this)
+  }
+
   render() {
-    return <p>Ciao a tutti!</p>
+    return (
+      <div>
+        <h1>Name That Tune!</h1>
+
+        {this.renderGenreList()}
+
+        {this.renderQuiz()}
+      </div>
+    )
+  }
+
+  genreToggle() {
+    this.setState({ showQuiz: !this.state.showQuiz })
+  }
+
+  renderGenreList() {
+    const genres = ['Classical', '60s', '80s']
+
+    if (!this.state.showQuiz) return <GenreList genres={genres} genreToggle={this.genreToggle}/>
+  }
+
+  renderQuiz() {
+    if (this.state.showQuiz) return <Quiz />
   }
 }
 
