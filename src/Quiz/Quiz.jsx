d3ba28c1087fe4363edc0a57bcc5b2263e0a62c2
Nick Litwin <nicklitwin88@gmail.com>
2016-11-17 21:35:38 -0800

    Show results after quiz has ended

--- src/Quiz/Quiz.jsx
+++ src/Quiz/Quiz.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import AnswerList from './AnswerList.jsx'
-import Audio from './Audio.jsx'
+import QuizQuestions from './QuizQuestions.jsx'
+import QuizResults from './QuizResults.jsx'
 
 import { fetchJSON, compileQuestions } from '../helpers'
 import { SPOTIFY_API } from '../constants'
@@ -11,11 +11,11 @@ class Quiz extends React.Component {
 
     this.state = {
       totalPoints: 0,
-      currentQuestionID: 0,
+      showResults: false,
       questions: null
     }
 
-    this.selectAnswer = this.selectAnswer.bind(this)
+    this.showResults = this.showResults.bind(this)
   }
 
   componentDidMount() {
@@ -30,47 +30,21 @@ class Quiz extends React.Component {
   }
 
   render() {
-    const { questions, currentQuestionID: id, totalPoints } = this.state
+    const { questions, showResults, totalPoints} = this.state
 
     if (!questions) {
-      return <div> Loading quiz...</div>
+      return <div>Loading quiz...</div>
     }
 
-    const currentQuestion = questions[id]
+    if (showResults) {
+      return <QuizResults points={totalPoints} />
+    }
 
-    return (
-      <div>
-        <h1>Choose the correct title</h1>
-
-        <p>{totalPoints}</p>
-
-        <Audio previewURL={currentQuestion.previewURL}/>
-
-        <AnswerList
-          selectAnswer={this.selectAnswer}
-          answers={currentQuestion.answers}
-        />
-      </div>
-    )
+    return <QuizQuestions questions={this.state.questions} onQuizEnd={this.showResults} />
   }
 
-  selectAnswer(selectedSongID) {
-    const { questions, currentQuestionID: id, totalPoints } = this.state
-    const currentQuestion = questions[id]
-
-    if (selectedSongID === currentQuestion.correctAnswerID) {
-      this.setState({ totalPoints: totalPoints + 100 })
-    }
-
-    if (id < questions.length - 1) {
-      this.setState({ currentQuestionID: id + 1 })
-
-    } else {
-      this.props.endQuiz()
-
-      // Set points in higher up component :o
-      alert(`Congratulations! You scored ${totalPoints} points!`)
-    }
+  showResults(totalPoints) {
+    this.setState({ showResults: true, totalPoints })
   }
 }
 
---
+++ src/Quiz/QuizResults.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const QuizResults = ({ points }) => {
+  return <div>Congratulations! You scored {points} points!</div>
+}
+
+export default QuizResults
